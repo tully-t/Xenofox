902f4e326c2f2c1f98f05ca4197d82ae0ec1f694
Tully Turk <159395530+tully-t@users.noreply.github.com>
2024-02-13 03:11:17 +0000

    Add platform-reponsive conditions to csd.css

---
+++ user.js
@@ -0,0 +1,22 @@
+/* Theme */
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.anchor_color", "#8500ff");
+user_pref("browser.display.background_color", "#1a1a1a");
+user_pref("browser.display.background_color.dark", "#1a1a1a");
+user_pref("browser.display.foreground_color", "#bbbbbb");
+user_pref("browser.display.foreground_color.dark", "#bbbbbb");
+user_pref("browser.visited_color", "#8500ff");
+user_pref("widget.content.allow-gtk-dark-theme", true);
+user_pref("gnomeTheme.systemIcons", false);
+user_pref("reader.color_scheme", "dark");
+user_pref("browser.newtabpage.activity-stream.showSearch", false);
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
+/*user_pref("widget.gtk.native-context-menus", true);*/
+/*****/
+
+/* Dark Reader enabled on restricted sites */
+user_pref("extensions.webextensions.restrictedDomains", "");
+user_pref("extensions.webextensions.addons-restricted-domains@mozilla.com.disabled", true);
+user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
+/*****/
