0e94c93e7e377ed7589286c0ab7ec509284dcc4a
Tully Turk <159395530+tully-t@users.noreply.github.com>
2024-11-27 15:53:01 -0800

    Update for Firefox 132
    - Tabs once again span the full width of the window properly & in line with the toolbar buttons
    - Now Playing icon on tabs is fixed
    - Window controls in CSD environments (Windows, MacOS, GNOME) are fixed

--- user.js
+++ user.js
@@ -1,7 +1,8 @@
 /* Theme */
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.aboutConfig.showWarning", false);
-user_pref("browser.anchor_color", "#8500ff");
+user_pref("browser.anchor_color", "#aaaaac20");
+user_pref("browser.anchor_color.dark", "#aaaaac20");
 user_pref("browser.display.background_color", "#1a1a1a");
 user_pref("browser.display.background_color.dark", "#1a1a1a");
 user_pref("browser.display.foreground_color", "#bbbbbb");
@@ -12,6 +13,8 @@ user_pref("gnomeTheme.systemIcons", false);
 user_pref("reader.color_scheme", "dark");
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("layout.css.prefers-color-scheme.content-override", 0);
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
 /*user_pref("widget.gtk.native-context-menus", true);*/
 /*****/
 
