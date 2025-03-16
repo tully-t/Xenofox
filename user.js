8fff8742ee8ab574d2fab115a9cbfcaddbf249fd
Tully Turk <159395530+tully-t@users.noreply.github.com>
2025-03-16 12:10:25 -0700

    Major update, menu glow
    - Menus and popup panels now have a green glow, with an option for purple glow
    - Tab previews, tooltips, findbar/Ctrl+f panel, and customization page are now all themed
    - Extensions menu has a new layout, icons only
    - Adjusted the blue-purple gradient used throughout to match the Xenon window decoration
    - Bookmarks/Places toolbar sizing now matches other toolbars and tab bar
    - Border added for Now Playing icon - improves visibility over brightly colored icons, e.g. YouTube
    - Tab throbber (loading dot) now hides under close button when hovered, mimicking favicon behavior
    - Menu separators are now hidden
    - Context menu bookmark icon now indicates if the page is bookmarked or not (a gap is present when the page is not bookmarked, which is filled in when the page is bookmarked)
    
    - Fixed issue with tab labels moving 1 pixel when hovered, but only sometimes, depending on mysterious factors like window width and number of tabs open
    - Fixed issue with pinned tabs where at a certain window width and number of tabs including a pinned one were open, the tab bar would shift left and right at glitch speed (There was a conflict between active tab length and tab bar margins)
    - Fixed container tab line margins, they are now centered on the favicon
    - Fixed titlebar button placement for those without titlebars (GTK Linux)
    
    - New option to remove some entries from the context menu
    - New optional rounded corners for web pages (adjustable, recommended for Xeno-gtk users and everyone who likes it)

--- user.js
+++ user.js
@@ -1,4 +1,4 @@
-/* Theme */
+/*** Theme ***/
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 user_pref("browser.aboutConfig.showWarning", false);
 user_pref("browser.anchor_color", "#aaaaac20");
@@ -13,13 +13,35 @@ user_pref("gnomeTheme.systemIcons", false);
 user_pref("reader.color_scheme", "dark");
 user_pref("browser.newtabpage.activity-stream.showSearch", false);
 user_pref("layout.css.prefers-color-scheme.content-override", 0);
-user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+// user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+// user_pref("widget.gtk.native-context-menus", true);
 
-/*user_pref("widget.gtk.native-context-menus", true);*/
-/*****/
 
-/* Dark Reader enabled on restricted sites */
+/*** Dark Reader enabled on restricted sites ***/
 user_pref("extensions.webextensions.restrictedDomains", "");
 user_pref("extensions.webextensions.addons-restricted-domains@mozilla.com.disabled", true);
 user_pref("privacy.resistFingerprinting.block_mozAddonManager", true);
-/*****/
+
+
+/*** Conveniences ***/
+
+/* KDE-specific */
+// user_pref("widget.non-native-theme.enabled", true); /* scrollbars */
+// user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
+// user_pref("widget.use-xdg-desktop-portal.location", 1);
+// user_pref("widget.use-xdg-desktop-portal.mime-handler", 1);
+// user_pref("widget.use-xdg-desktop-portal.open-uri", 1);
+// user_pref("widget.use-xdg-desktop-portal.settings", 1);
+// user_pref("widget.gtk.global-menu.enabled", true);
+// user_pref("widget.gtk.global-menu.wayland.enabled", true);
+
+/* Zoom Restriction */
+user_pref("zoom.minPercent", 100);
+
+/* Backspace to go back one page */
+user_pref("browser.backspace_action", 0);
+
+/* Autoplay */
+user_pref("media.block-autoplay-until-in-foreground", true);
+user_pref("media.block-play-until-document-interaction", true);
+user_pref("media.block-play-until-visible", true);
